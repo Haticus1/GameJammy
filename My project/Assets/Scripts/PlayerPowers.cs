c4541da7293e4342d7adc0a6b1b1106c53bf81ca
JWatts1402 <JonathanWatts1402@gmail.com>
2023-04-30 18:44:54 +0100

    added first iteration of time slow
    it kinda sucks but its there :)

---
+++ My project/Assets/Scripts/PlayerPowers.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPowers : MonoBehaviour
+{
+    [SerializeField] float SlowScale = 0.6f;
+    [SerializeField] float slowMeter = 10;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            TimeSlow();
+        }
+        else
+        {
+            if (slowMeter < 10)
+            {
+                slowMeter += Time.deltaTime;
+            }
+            else if (slowMeter > 10) slowMeter = 10;          
+        }
+    }
+
+    void TimeSlow()
+    {
+        if (slowMeter > 0)
+        {
+            slowMeter -= Time.deltaTime; 
+            Time.timeScale = SlowScale;
+        }
+        else Time.timeScale = 1;        
+    }
+}
